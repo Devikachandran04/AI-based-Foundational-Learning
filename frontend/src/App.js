a2bf3385ed92108dee95e1bddb32f19e688ee67e
Ardra A S <ardraas2005@gmail.com>
2026-03-23 13:44:45 +0530

    additional admin login page is removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,27 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import TeacherDashboard from "./teacher/pages/TeacherDashboard";
 import ProgressDetails from "./teacher/pages/ProgressDetails";
 import WeakTopicsDetails from "./teacher/pages/WeakTopicsDetails";
 import LowScoreDetails from "./teacher/pages/LowScoreDetails";
 import HelpRequestsDetails from "./teacher/pages/HelpRequestsDetails";
-import AdminLogin from "./teacher/pages/AdminLogin";
-
-import ProtectedRoute from "./ProtectedRoute";
 import LearnerProfile from "./learner/learnerprofile";
+
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Default route */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
 
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <TeacherDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/progress"
-          element={
-            <ProtectedRoute>
-              <ProgressDetails />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/weak-topics"
-          element={
-            <ProtectedRoute>
-              <WeakTopicsDetails />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/low-scores"
-          element={
-            <ProtectedRoute>
-              <LowScoreDetails />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/help-requests"
-          element={
-            <ProtectedRoute>
-              <HelpRequestsDetails />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/dashboard" element={<TeacherDashboard />} />
+        <Route path="/progress" element={<ProgressDetails />} />
+        <Route path="/weak-topics" element={<WeakTopicsDetails />} />
+        <Route path="/low-scores" element={<LowScoreDetails />} />
+        <Route path="/help-requests" element={<HelpRequestsDetails />} />
         <Route path="/learner-profile" element={<LearnerProfile />} />
-
       </Routes>
     </Router>
   );
