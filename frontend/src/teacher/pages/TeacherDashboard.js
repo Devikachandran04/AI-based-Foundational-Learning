379a32bb58f7f48febadcc177a21a600a5484beb
Devika Chandran D <devika6124dka@gmail.com>
2026-03-26 22:52:04 +0530

    teacher dashboard modifying

--- frontend/src/teacher/pages/TeacherDashboard.js
+++ frontend/src/teacher/pages/TeacherDashboard.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FiLogOut } from "react-icons/fi";
 import { useEffect, useState } from "react";
 import axios from "axios";
